66654e4819339e92406eabd72e1eea187e54026d
AddisonWolfeJMU <wolfeap@dukes.jmu.edu>
2025-09-24 15:27:48 -0400

    prompt changes

--- llm_interface/prompt_creator.py
+++ llm_interface/prompt_creator.py
@@ -1,12 +1,12 @@
 from lexer.lexer import lex, group_lex, Component
 
 FRAMEWORK = "React"
-PROMPT_BASE = f"Please create the following components using {FRAMEWORK}. Please only return code:\n"
+PROMPT_BASE = "Generate React JSX code for the following components. Do NOT describe them in words. Only return JSX code. Include proper props and styles.:\n"
 
 def create_prompt(text: str):
     tokens = lex(text)
     components = group_lex(tokens)
     prompt = PROMPT_BASE
-    prompt += "A red button with black text"
+    prompt += "Red button with black text \"Sign Up\""
     return prompt
 
